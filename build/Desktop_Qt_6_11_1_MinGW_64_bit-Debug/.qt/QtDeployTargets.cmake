72f7f385112ed79fff0f6a35176bf42ddf0e42bb
Misha-T1908 <59511154+Misha-T1908@users.noreply.github.com>
2026-06-04 23:03:39 +0200

    Initial commit

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Scoreboard_FILE C:/Users/tolst/Documents/Scoreboard/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/Scoreboard.exe)
+set(__QT_DEPLOY_TARGET_Scoreboard_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Scoreboard_RUNTIME_DLLS C:/Users/tolst/Documents/QT/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Users/tolst/Documents/QT/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Users/tolst/Documents/QT/6.11.1/mingw_64/bin/Qt6Core.dll)
